9d8898fc3e2f7c21a661a6db91a38b227855a430
wxd56987 <672514143@qq.com>
2024-05-19 17:15:42 +0800

    feat: add interface MarketState

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-// index.ts
+import BigNumber from "bignumber.js";
+export interface MarketState {
+  totalPt: BigNumber;
+  totalSy: BigNumber;
+  totalLp: BigNumber;
+  treasury: `0x${string}`;
+  scalarRoot: BigNumber;
+  expiry: BigNumber;
+  lnFeeRateRoot: BigNumber;
+  reserveFeePercent: BigNumber;
+  lastLnImpliedRate: BigNumber;
+  lifecircle: BigNumber;
+  sAPR: BigNumber;
+}
